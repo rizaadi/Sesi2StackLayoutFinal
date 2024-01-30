6964f15fc8c071a48d87e9ac6da90e2f2fe926e9
rizaadi <rizaadi890@gmail.com>
2024-01-30 19:41:14 +0700

    Initial Commit

---
+++ Sesi2StackLayoutFinal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sesi2StackLayoutFinal
+//
+//  Created by Riza Adi Kurniawan on 30/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sesi2StackLayoutFinal/Sesi2StackLayoutFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sesi2StackLayoutFinalApp.swift
+//  Sesi2StackLayoutFinal
+//
+//  Created by Riza Adi Kurniawan on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Sesi2StackLayoutFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
